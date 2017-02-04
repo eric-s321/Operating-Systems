54012b8379a8c879391e4fa04871e8a1894af05c
Eric Scagnelli <ericscagnelli@gmail.com>
2017-02-04 03:36:55 -0500

    Basics of process tree working.  Need to implement sleep and pause functionality

--- Prog2Scagnelli_escagne1/prog2tree.c
+++ Prog2Scagnelli_escagne1/prog2tree.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <getopt.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 
 
@@ -74,16 +77,30 @@ void checkRangeOfInputs(int numLevels, int numChildren){
     }
 }
 
-/*
-void createProcesses(int numLevels, int numChildren, bool leafPause, int sleepTime){
-        
-
-}
-*/
-
-
 
 int main(int argc, char *argv[]){
+
+/////////////////////////USE THIS TO AVOID HARDCODING THE PATH ////////////////////////////////////
+//Found at http://stackoverflow.com/questions/298510/how-to-get-the-current-directory-in-a-c-program
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+        fprintf(stdout, "Current working dir: %s\n", cwd);
+
+    for(int i = 0; cwd[i] != '\0'; i++){
+        printf("%c",cwd[i]);    
+    }
+    printf("\n");
+
+    /*
+    printf("\n\n");
+    printf("argc is %d\n", argc);
+    for(int i = 0; i < argc; i++){
+        printf("%s\t", argv[i]);
+    }
+    printf("\n\n");
+    */
+    
+
     //Set default values
     int numLevels = 0;
     int numChildren = 1;
@@ -94,11 +111,47 @@ int main(int argc, char *argv[]){
 
     checkRangeOfInputs(numLevels, numChildren);
 
+/*
     printf("numLevels is %d\n"
         "numChildren is %d\n"
         "leafPause is %s\n"
         "sleepTime is %d\n",
     numLevels, numChildren,
     leafPause ? "true" : "false", sleepTime);    
+*/
+    
+    fprintf(stdout,"ALIVE: Level %d process with pid=%d, child of ppid=%d.\n",
+            numLevels - 1, getpid(), getppid());
 
+    pid_t pid;
+
+    if(numLevels > 1){ // This process still has children to make
+        for(int i = 0; i < numChildren; i++){
+            pid = fork();
+            if(pid < 0){
+                fprintf(stderr, "Fork Failed.\n");
+                return 1;
+            }
+            if(pid == 0){ //Child process code
+                char newNumLevels[255];
+                char strNumChildren[255]; 
+                sprintf(newNumLevels, "%d", (numLevels - 1));
+                sprintf(strNumChildren, "%d", numChildren);
+                execlp("/import/linux/home/escagne1/Desktop/Operating-Systems/Prog2Scagnelli_escagne1/prog2tree",
+                           "prog2tree", "-N", newNumLevels, "-M", strNumChildren, NULL); 
+            }
+            else{ //Parent Process
+                wait(NULL);
+                printf("Child has completed.\n"); 
+            }
+        }
+    }
+
+    else{ //This process is a leaf node.
+        printf("I am a leaf node. My pid is %d and my parents pid is %d\n", getpid(), getppid());
+    }
+    
+    fprintf(stdout, "EXITING: Level %d process with pid=%d, child of ppid=%d.\n",
+                numLevels - 1, getpid(), getppid()); 
+    return 0;
 }
